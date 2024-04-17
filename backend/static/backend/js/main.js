08fdd348984a6f63b2e3352988aa812fb9c0ac32
Vyacheslav Krokhalev <krox1337@yandex.ru>
2024-04-17 21:45:32 +0300

    сверстаны вопросы с каруселью

--- backend/static/backend/js/main.js
+++ backend/static/backend/js/main.js
@@ -7,3 +7,18 @@ $('.carousel').slick({
   fade: true,
   cssEase: 'linear'
 });
+
+
+$('.carousel-questions').slick({
+  autoplay: true,
+  autoplaySpeed: 5000,
+  dots: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  centerPadding: '40px',
+  // centerMode: true,
+  // variableWidth: true
+
+});
