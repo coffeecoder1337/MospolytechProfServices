07b21e6947f1c7355b6e9174e39e2c606fd31672
Vyacheslav Krokhalev <krox1337@yandex.ru>
2024-04-19 21:42:51 +0300

    добавлен эффект появления текста при наведении на новости и вопросы. Поправлена верстка хедера и новостей

--- backend/static/backend/js/main.js
+++ backend/static/backend/js/main.js
@@ -13,12 +13,34 @@ $('.carousel-questions').slick({
   autoplay: true,
   autoplaySpeed: 5000,
   dots: true,
+  arrows: true,
   infinite: true,
   speed: 500,
   slidesToShow: 3,
   slidesToScroll: 3,
-  centerPadding: '40px',
-  // centerMode: true,
+  // centerPadding: '40px',
+  centerMode: true,
   // variableWidth: true
 
 });
+
+
+
+$( ".news-card" ).hover(
+  function() {
+    $(this).find(".news-description").css( "opacity", "1" );
+  }, function() {
+    $(this).find(".news-description").css( "opacity", "0" );
+  }
+);
+
+
+$( ".questions-card" ).hover(
+  function() {
+    $(this).find(".questions-description").css( "opacity", "1" );
+    $(this).find(".question").css( "opacity", "0" );
+  }, function() {
+    $(this).find(".questions-description").css( "opacity", "0" );
+    $(this).find(".question").css( "opacity", "1" );
+  }
+);
